e429fd41298fd187124189537b705ddce99aff89
kill0rz <kill0rz@users.noreply.github.com>
2017-06-15 11:42:02 +0200

    install script

--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ chmod +x forcepull.sh
 echo "config.php" >> .gitignore
 
 #chrisify
-sudo apt install -yy libopencv-dev
+sudo apt update && apt install -yy libopencv-dev golang
+export PATH=$PATH:/usr/local/go/bin
+mkdir ~/go
+export GOPATH=~/go
+export PATH=$PATH:$GOPATH/bin
 go get github.com/zikes/chrisify
 go get github.com/lazywei/go-opencv
 cd $GOPATH/src/github.com/zikes/chrisify
 go build
 cd ..
-cp -R chrisify/ cwd/
+cp -R chrisify/ $cwd/

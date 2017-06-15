7b166609a72bbfef08691a96bfdd6c7a631f1f20
kill0rz <kill0rz@users.noreply.github.com>
2017-06-15 12:06:30 +0200

    updated update script

--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-git stash
-git stash drop
+chmod -x update.sh
 
 git update-index --assume-unchanged config.php
 git update-index --assume-unchanged chrisify/
+git update-index --assume-unchanged bot_modules/prio2/haffkrug.php
+git update-index --assume-unchanged bot_modules/prio1/03_lachsticker.php
+
+#git stash
+#git stash drop
 
 git pull
 

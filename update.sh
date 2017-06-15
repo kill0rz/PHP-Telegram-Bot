c2c336c942fcf3a98257c71e2ada912033111687
kill0rz <kill0rz@users.noreply.github.com>
2017-06-15 17:25:34 +0200

    updated update script

--- update.sh
+++ update.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-chmod -x update.sh
-
 git update-index --assume-unchanged config.php
 git update-index --assume-unchanged chrisify/
 git update-index --assume-unchanged bot_modules/prio2/haffkrug.php
 git update-index --assume-unchanged bot_modules/prio1/03_lachsticker.php
 
-git fetch
-git checkout origin/master update.sh
+#git fetch
+#git checkout origin/master update.sh
 
 git pull
-
-#rights
-chmod +x update.sh

b8bba4a4afd489abad6f02eb9e37279fe59a3c6d
kill0rz <kill0rz@users.noreply.github.com>
2017-10-20 18:23:46 +0200

    postinterval: added posibility to send a random document when sticker and video isset

--- post_interval.php
+++ post_interval.php
@@ -9,10 +9,18 @@ if (isset($post_interval_text) && trim($post_interval_text) != '') {
 	post_reply($post_interval_text);
 }
 
-if (isset($post_interval_video_id) && trim($post_interval_video_id) != '') {
-	send_document($post_interval_video_id);
-}
+if (isset($post_interval_video_id) && trim($post_interval_video_id) != '' && isset($post_interval_sticker_id) && trim($post_interval_sticker_id) != '') {
+	if (rand(0, 1) == 0) {
+		send_document($post_interval_video_id);
+	}else{
+		send_sticker($post_interval_sticker_id);
+	}
+} else {
+	if (isset($post_interval_video_id) && trim($post_interval_video_id) != '') {
+		send_document($post_interval_video_id);
+	}
 
-if (isset($post_interval_sticker_id) && trim($post_interval_sticker_id) != '') {
-	send_sticker($post_interval_sticker_id);
+	if (isset($post_interval_sticker_id) && trim($post_interval_sticker_id) != '') {
+		send_sticker($post_interval_sticker_id);
+	}
 }

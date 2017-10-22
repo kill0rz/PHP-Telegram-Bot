301342528b76458fa270a04c758b9dd85f73a577
kill0rz <kill0rz@users.noreply.github.com>
2017-10-22 12:50:41 +0200

    fix: PHP Notice:  Only variables should be passed by reference

--- bot_modules/prio1/02_triggerfilter.php
+++ bot_modules/prio1/02_triggerfilter.php
@@ -2,14 +2,14 @@
 
 // Trigger-Filter
 foreach ($triggersticker as $triggerword => $triggerstickerid) {
-	if (isset($update["message"]["text"]) && preg_match($triggerword, "", strtolower($update["message"]["text"]))) {
+	if (isset($update["message"]["text"]) && preg_match($triggerword, strtolower($update["message"]["text"]))) {
 		send_sticker($triggerstickerid);
 		exit();
 	}
 }
 
 foreach ($triggersticker_perperson as $triggersticker) {
-	if (isset($update["message"]["text"]) && preg_match($triggersticker['searchpattern'], "", strtolower($update["message"]["text"])) && $update["message"]["from"]["id"] == $triggersticker['userid']) {
+	if (isset($update["message"]["text"]) && preg_match($triggersticker['searchpattern'], strtolower($update["message"]["text"])) && $update["message"]["from"]["id"] == $triggersticker['userid']) {
 		send_sticker($triggersticker['stickerid']);
 		exit();
 	}

d712eb6a977196c7edfa249b44a7c69c2b7a1533
kill0rz <kill0rz@kill0rz.com>
2018-01-07 15:28:44 +0100

    fixed null vars

--- bot_modules/prio1/02_triggerfilter.php
+++ bot_modules/prio1/02_triggerfilter.php
@@ -1,16 +1,20 @@
 <?php
 
 // Trigger-Filter
-foreach ($triggersticker as $triggerword => $triggerstickerid) {
-	if (isset($update["message"]["text"]) && preg_match($triggerword, strtolower($update["message"]["text"]))) {
-		send_sticker($triggerstickerid);
-		exit();
+if (isset($triggersticker) && count($triggersticker) > 0) {
+	foreach ($triggersticker as $triggerword => $triggerstickerid) {
+		if (isset($update["message"]["text"]) && preg_match($triggerword, strtolower($update["message"]["text"]))) {
+			send_sticker($triggerstickerid);
+			exit();
+		}
 	}
 }
 
-foreach ($triggersticker_perperson as $triggersticker) {
-	if (isset($update["message"]["text"]) && preg_match($triggersticker['searchpattern'], strtolower($update["message"]["text"])) && $update["message"]["from"]["id"] == $triggersticker['userid']) {
-		send_sticker($triggersticker['stickerid']);
-		exit();
+if (isset($triggersticker_perperson) && count($triggersticker_perperson) > 0) {
+	foreach ($triggersticker_perperson as $triggersticker) {
+		if (isset($update["message"]["text"]) && preg_match($triggersticker['searchpattern'], strtolower($update["message"]["text"])) && $update["message"]["from"]["id"] == $triggersticker['userid']) {
+			send_sticker($triggersticker['stickerid']);
+			exit();
+		}
 	}
 }

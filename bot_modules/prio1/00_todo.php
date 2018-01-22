0cffb0620207a222d379d13c5713f16fdd96d658
kill0rz <kill0rz@kill0rz.com>
2018-01-22 16:29:39 +0100

    added possibility to change ticket owner; some error handling

--- bot_modules/prio1/00_todo.php
+++ bot_modules/prio1/00_todo.php
@@ -25,8 +25,9 @@ if (isset($chatID) && ($chatID == $todo_chatID)) {
 				$text .= "Hilfe der ToDo-Gruppe:\n\n";
 				$text .= "/help --> Diese Hilfe hier\n";
 				$text .= "+ {TEXT} --> Ticket hinzufügen\n";
-				$text .= "*# {NUMMER} --> Ticket als 'In Arbeit' kennzeichnen\n";
-				$text .= "-# {NUMMER} --> Ticket entfernen\n";
+				$text .= "*#{NUMMER} --> Ticket als 'In Arbeit' kennzeichnen\n";
+				$text .= "-#{NUMMER} --> Ticket entfernen\n";
+				$text .= "~#{NUMMER} --> Ticket jemand anderem zuweisen\n";
 				post_reply($text);
 				break;
 
@@ -46,27 +47,31 @@ if (isset($chatID) && ($chatID == $todo_chatID)) {
 				if ($update["message"]["from"]["id"] == $admin_id) {
 					// find ticketid
 					$tmp = preg_match_all("/#\s?([0-9]*)/", trim($update["message"]["text"]), $matches);
-					$ticketID = $matches[1][0];
+					if (isset($matches[1][0])) {
+						$ticketID = $matches[1][0];
 
-					$sql = "SELECT * FROM tb_todolist WHERE ID=" . intval($ticketID) . " LIMIT 1; --";
-					$result = $mysqli->query($sql);
-					if ($result->num_rows == 0) {
-						post_reply("Ticket wurde nicht gefunden.");
-					} else {
-						while ($row = $result->fetch_object()) {
-							if ($row->isactive == 1) {
-								// set inactive
-								$sql = "UPDATE tb_todolist SET isactive = 0, isintodo = 0 WHERE ID=" . intval($ticketID) . "; --";
-								$mysqli->query($sql);
-								$result_atname = $mysqli->query("SELECT atname FROM tb_lastseen_users JOIN tb_todolist ON tb_todolist.wishby = tb_lastseen_users.userid WHERE tb_todolist.ID='" . intval($ticketID) . "';");
-								$atname = $result_atname->fetch_array();
-								$atname = (isset($atname['atname']) && trim($atname['atname']) != '') ? "@" . $atname['atname'] : '';
-								post_todotable("Ticket #" . intval($ticketID) . " (\"" . $row->content . "\") wurde geschlossen! " . $atname);
-							} else {
-								// is inactive
-								post_reply("Ticket bereits abgearbeitet!");
+						$sql = "SELECT * FROM tb_todolist WHERE ID=" . intval($ticketID) . " LIMIT 1; --";
+						$result = $mysqli->query($sql);
+						if ($result->num_rows == 0) {
+							post_reply("Ticket wurde nicht gefunden.");
+						} else {
+							while ($row = $result->fetch_object()) {
+								if ($row->isactive == 1) {
+									// set inactive
+									$sql = "UPDATE tb_todolist SET isactive = 0, isintodo = 0 WHERE ID=" . intval($ticketID) . "; --";
+									$mysqli->query($sql);
+									$result_atname = $mysqli->query("SELECT atname FROM tb_lastseen_users JOIN tb_todolist ON tb_todolist.wishby = tb_lastseen_users.userid WHERE tb_todolist.ID='" . intval($ticketID) . "';");
+									$atname = $result_atname->fetch_array();
+									$atname = (isset($atname['atname']) && trim($atname['atname']) != '') ? "@" . $atname['atname'] : '';
+									post_todotable("Ticket #" . intval($ticketID) . " (\"" . $row->content . "\") wurde geschlossen! " . $atname);
+								} else {
+									// is inactive
+									post_reply("Ticket bereits abgearbeitet!");
+								}
 							}
 						}
+					} else {
+						post_reply("Es wurde keine Ticketnummer angegeben!");
 					}
 				} else {
 					post_reply("Sorry, das darf nur der Admin!");
@@ -78,26 +83,49 @@ if (isset($chatID) && ($chatID == $todo_chatID)) {
 				if ($update["message"]["from"]["id"] == $admin_id) {
 					// find ticketid
 					$tmp = preg_match_all("/#\s?([0-9]*)/", trim($update["message"]["text"]), $matches);
-					$ticketID = $matches[1][0];
+					if (isset($matches[1][0])) {
+						$ticketID = $matches[1][0];
 
-					$sql = "SELECT * FROM tb_todolist WHERE ID=" . intval($ticketID) . " LIMIT 1; --";
-					$result = $mysqli->query($sql);
-					if ($result->num_rows == 0) {
-						post_reply("Ticket wurde nicht gefunden.");
-					} else {
-						while ($row = $result->fetch_object()) {
-							if ($row->isactive == 1) {
-								// set inactive
-								$sql = "UPDATE tb_todolist SET isintodo = 1 WHERE ID=" . intval($ticketID) . "; --";
-								$mysqli->query($sql);
-								$result_atname = $mysqli->query("SELECT atname FROM tb_lastseen_users JOIN tb_todolist ON tb_todolist.wishby = tb_lastseen_users.userid WHERE tb_todolist.ID='" . intval($ticketID) . "';");
-								$atname = $result_atname->fetch_array();
-								$atname = (isset($atname['atname']) && trim($atname['atname']) != '') ? "@" . $atname['atname'] : '';
-								post_todotable("Ticket #" . intval($ticketID) . " (\"" . $row->content . "\") ist nun in Arbeit. " . $atname);
-							} else {
-								// is inactive
-								post_reply("Ticket bereits abgearbeitet!");
+						$sql = "SELECT * FROM tb_todolist WHERE ID=" . intval($ticketID) . " LIMIT 1; --";
+						$result = $mysqli->query($sql);
+						if ($result->num_rows == 0) {
+							post_reply("Ticket wurde nicht gefunden.");
+						} else {
+							while ($row = $result->fetch_object()) {
+								if ($row->isactive == 1) {
+									// set inactive
+									$sql = "UPDATE tb_todolist SET isintodo = 1 WHERE ID=" . intval($ticketID) . "; --";
+									$mysqli->query($sql);
+									$result_atname = $mysqli->query("SELECT atname FROM tb_lastseen_users JOIN tb_todolist ON tb_todolist.wishby = tb_lastseen_users.userid WHERE tb_todolist.ID='" . intval($ticketID) . "';");
+									$atname = $result_atname->fetch_array();
+									$atname = (isset($atname['atname']) && trim($atname['atname']) != '') ? "@" . $atname['atname'] : '';
+									post_todotable("Ticket #" . intval($ticketID) . " (\"" . $row->content . "\") ist nun in Arbeit. " . $atname);
+								} else {
+									// is inactive
+									post_reply("Ticket bereits abgearbeitet!");
+								}
 							}
+						}
+					} else {
+						post_reply("Es wurde keine Ticketnummer angegeben!");
+					}
+				} else {
+					post_reply("Sorry, das darf nur der Admin!");
+				}
+				break;
+
+			case "~":
+				if ($update["message"]["from"]["id"] == $admin_id) {
+					preg_match_all("~#(\d+)\s@(\S*)", trim($update["message"]["text"]), $matches);
+					if (isset($matches[0][2])) {
+						$ticketId = $matches[0][1];
+						$newUsername = $matches[0][2];
+						$sql = "UPDATE tb_todolist SET wishby = (SELECT userid FROM tb_lastseen_users WHERE atname = '{$newUsername}' LIMIT 1) WHERE ID='{$ticketId}'";
+						$mysqli->query($sql);
+						if ($mysqli->error != '') {
+							post_reply("Datenbankfehler! @" . $admin_name . "\n" . $mysqli->error);
+						} else {
+							post_reply("Ticket {$ticketId} gehört nun {$newUsername}.");
 						}
 					}
 				} else {
